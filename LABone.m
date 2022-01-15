9b4a8b82082f1d5cfc16058ee9f2c5891c76d2fd
Alice Catalano <91795156+AliceCatalano@users.noreply.github.com>
2022-01-15 18:05:18 +0000

    Add files via upload

---
+++ LABone.m
@@ -0,0 +1,10 @@
+startup_rvc
+%links should be modfied as in table
+L(1)=Link([0 108 0 -(90*pi/180)])
+L(2)=Link([0 0 0 -(90*pi/180)])
+L(3)=Link([0 431.5 145.5 (90*pi/180)])
+L(4)=Link([0 0 0 -(90*pi/180)])
+L(5)=Link([0 455 0 -(90*pi/180)])
+L(6)=Link([0 0 0 -(90*pi/180)])
+L(7)=Link([0 153 0 0])
+Rob=SerialLink(L)
